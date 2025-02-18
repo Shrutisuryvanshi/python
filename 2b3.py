796401fe0d5f69a0e2ab58eebb700924c06d1628
Shrutisuryvanshi <shrutisuryavanshi315@gmail.com>
2025-02-18 10:19:07 +0530

    Add files via upload

--- 2b3.py
+++ 2b3.py
@@ -1,13 +1,4 @@
-def remove_odd_index_characters(input_string):
-    
-    result = "".join(input_string[i] for i in range(len(input_string))if i % 2 ==0)
-    return result
-
-
-input_string = input("enter a string:")
-
-
-result = remove_odd_index_charecters(input_string)
-
-
-print(f"string after removing cha racter with odd index values: {result}")
+a="shruti"
+for i in range(len(a)):
+    if((i%2)!=0):
+        print(a[i],end=" ")
