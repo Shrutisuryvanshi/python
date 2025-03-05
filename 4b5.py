9fc0616f32e18e29b85d1243703ee8cb589e147a
Shrutisuryvanshi <shrutisuryavanshi315@gmail.com>
2025-03-05 10:36:26 +0530

    Add files via upload

---
+++ 4b5.py
@@ -0,0 +1,10 @@
+set1 = {1, 2, 3, 4, 5}
+set2 = {4, 5, 6, 7, 8}
+
+
+set_difference = set1.difference(set2)
+print("Set Difference (set1.difference(set2)):", set_difference)
+
+
+symmetric_difference = set1.symmetric_difference(set2)
+print("Symmetric Difference (set1.symmetric_difference(set2)):", symmetric_difference)
