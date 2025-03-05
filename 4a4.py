0e7e3b5c2db1cc0e3dccae1c12e2b41afb9dbcd0
Shrutisuryvanshi <shrutisuryavanshi315@gmail.com>
2025-03-05 09:36:25 +0530

    Add files via upload

---
+++ 4a4.py
@@ -0,0 +1,13 @@
+set={1,2,3,4,5}
+print(set)
+e=int(input(
+    "enter element to add in set"))
+set.add(e)
+
+print(f"set after adding:",set)
+
+r=int(input(
+    "enter element to remove in set"))
+set.remove(r)
+
+print("set after removing:",set)
