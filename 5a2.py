2770578f951e10750c71667ab1e91ff787b9ac11
Shrutisuryvanshi <shrutisuryavanshi315@gmail.com>
2025-03-09 21:11:58 -0700

    Add files via upload

---
+++ 5a2.py
@@ -0,0 +1,10 @@
+# Sample dictionaries
+dic1 = {1: 10, 2: 20}
+dic2 = {3: 30, 4: 40}
+dic3 = {5: 50, 6: 60}
+
+# Concatenating dictionaries
+result = {**dic1, **dic2, **dic3}
+
+# Printing the result
+print("Concatenated Dictionary:", result)
