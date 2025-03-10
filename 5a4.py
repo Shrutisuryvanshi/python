55ab689ffe4a71de25122f91ff0cd13bf4d6e8a6
Shrutisuryvanshi <shrutisuryavanshi315@gmail.com>
2025-03-09 21:12:28 -0700

    Add files via upload

---
+++ 5a4.py
@@ -0,0 +1,8 @@
+# Sample dictionary
+m_d = {'data1': 100, 'data2': -54, 'data3': 247}
+
+# Summing all values
+result = sum(m_d.values())
+
+# Printing the result
+print("Sum of all values:", result)
