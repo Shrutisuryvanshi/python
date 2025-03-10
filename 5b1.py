105144f5957428c955fcc7930f1b87e25073244e
Shrutisuryvanshi <shrutisuryavanshi315@gmail.com>
2025-03-09 21:13:12 -0700

    Add files via upload

---
+++ 5b1.py
@@ -0,0 +1,10 @@
+color_dict = {
+    'red': '#ff0000',
+    'green': '#008000',
+    'black': '#000000',
+    'white': '#ffffff'
+}
+
+
+for key in sorted(color_dict):
+    print(f"{key}: {color_dict[key]}")
