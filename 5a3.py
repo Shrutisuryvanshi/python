57cbcbaf6994e3766715e87c052f25b9a3a7a206
Shrutisuryvanshi <shrutisuryavanshi315@gmail.com>
2025-03-09 21:12:12 -0700

    Add files via upload

---
+++ 5a3.py
@@ -0,0 +1,6 @@
+# Sample dictionary
+my_dict = {"name": "Alice", "age": 25, "city": "New York"}
+
+# Iterating through keys and values
+for key, value in my_dict.items():
+    print(f"{key}: {value}")
