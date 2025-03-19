f45f44f099d52ddf725401c37845f18775f71451
Shrutisuryvanshi <shrutisuryavanshi315@gmail.com>
2025-03-18 21:10:48 -0700

    Add files via upload

---
+++ 6a4.py
@@ -0,0 +1,7 @@
+def show_employee(name, salary=9000):
+    print(f"Employee Name: {name}")
+    print(f"Employee Salary: {salary}")
+
+
+show_employee("Vidya", 12000)  
+show_employee("Mayuri")        
